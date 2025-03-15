c4eccaf8b3b68ec5727fb25ad7c19c3073999471
Andres Schuster <andi.schus@live.com.ar>
2025-03-15 11:44:12 -0300

    Initial commit

---
+++ backend/src/main/java/com/example/clicker_game/ClickerGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.clicker_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickerGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickerGameApplication.class, args);
+	}
+
+}
